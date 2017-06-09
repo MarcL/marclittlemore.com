529a766104ed9a765568354990cb736c39192118
Marc Littlemore <marc.littlemore@bbc.co.uk>
2017-06-09 22:40:07 +0100

    Move JS into src and add build step

---
+++ src/mailChimpTracking.js
@@ -0,0 +1,21 @@
+var URLSearchParams = require('url-search-params');
+
+function appendSignupLocation(formId) {
+    var SIGNUP_KEY = 'signup';
+
+    var urlParams = new URLSearchParams(window.location.search);
+    signUpCode = urlParams.get(SIGNUP_KEY);
+
+    if (signUpCode) {
+        var mailChimpForm = document.getElementById(formId);
+        if (mailChimpForm) {
+            var input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = 'SIGNUP';
+            input.value = signUpCode;
+            mailChimpForm.appendChild(input);
+        }
+    }
+}
+
+module.exports = appendSignupLocation;
