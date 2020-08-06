0f3a4241cead94cb5f5e63a6cbfeac46672909dc
Marc Littlemore <marc.littlemore@gmail.com>
2020-08-06 09:53:30 +0100

    Add shortcode for quotes

--- .eleventy.js
+++ .eleventy.js
@@ -75,6 +75,12 @@ module.exports = (eleventyConfig) => {
         );
     });
 
+    // TODO: Need markdown rendering?
+    eleventyConfig.addPairedShortcode('quote', (content) => {
+        const renderedHtml = markdownLib.render(content);
+        return `<blockquote class="helvetica ml0 mt0 pl4 black-90 bl bw2 b--blue f5 f4-m f3-l lh-copy measure">${renderedHtml}</blockquote>`;
+    });
+
     // Liquid template options
     eleventyConfig.setLiquidOptions({
         dynamicPartials: false,
