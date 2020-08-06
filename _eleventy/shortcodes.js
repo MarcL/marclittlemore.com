4c89b45dd5c353e9a89ecb3427d3e3920e29e80b
Marc Littlemore <marc.littlemore@gmail.com>
2020-08-06 16:18:03 +0100

    Add icon to callout and style properly

--- _eleventy/shortcodes.js
+++ _eleventy/shortcodes.js
@@ -2,14 +2,25 @@ const markdownLib = require('markdown-it')();
 
 const callout = (content, type = 'info') => {
     const typeClasses = {
-        info: 'bg-washed-green b--dark-green',
-        warning: 'bg-washed-red b--dark-red',
-        tip: 'bg-washed-blue b--dark-blue',
+        info: {
+            background: 'bg-washed-green b--dark-green',
+            icon: 'fa-info-circle dark-green'
+        },
+        warning: {
+            background: 'bg-washed-red b--dark-red',
+            icon: 'fa-exclamation-circle dark-red'
+        },
+        tip: {
+            background: 'bg-light-blue b--dark-blue',
+            icon: 'fa-lightbulb dark-blue'
+        }
     };
 
-    const chosenTypeClass = typeClasses[type] || typeClasses['info'];
+    const chosenType = typeClasses[type] || typeClasses['info'];
     const renderedHtml = markdownLib.render(content);
-    return `<div class="fw5 bl bw2 ph3 pv2 ${chosenTypeClass}">${renderedHtml}</div>`
+
+    const iconInfo = `<i class="fa absolute top-0 left-0 pa1 bg-white br-100 shadow-4 ${chosenType.icon}" style="transform: translate(-50%,-50%)"></i>`;
+    return `<div class="fw5 bl bw2 br2 shadow-4 ph3 pv2 mv2 relative ${chosenType.background}">${iconInfo}${renderedHtml}</div>`
 };
 
 const quote = (content) => {
