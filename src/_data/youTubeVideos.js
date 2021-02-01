14a62b91354ec5495609e2e4f5af12345d99f0f6
Marc Littlemore <MarcL@users.noreply.github.com>
2021-02-01 12:10:05 +0000

    Cache YouTube playlists (#67)

--- src/_data/youTubeVideos.js
+++ src/_data/youTubeVideos.js
@@ -1,4 +1,5 @@
 const {google} = require('googleapis');
+const {AssetCache} = require("@11ty/eleventy-cache-assets");
 
 const youTubeService = google.youtube({
     version: 'v3',
@@ -7,14 +8,31 @@ const youTubeService = google.youtube({
 
 const MESSENGER_MARKETING_PLAYLIST_ID = 'PLDvWRKT9Cd2g-L4_hStYOcmOfTF87U8FM';
 
-const getYouTubeVideos = async () => {
-    try {
-        const response = await youTubeService.playlistItems.list({
-            playlistId: MESSENGER_MARKETING_PLAYLIST_ID,
-            part: 'id,snippet',
-            maxResults: 50
-        });
+const getCachedPlaylist = async (id) => {
+    const asset = new AssetCache(`youtube-playlist-${id}`);
     
+    if (asset.isCacheValid('1d')) {
+        console.log(`Loading YouTube playlist '${id}' from cache`)
+        return asset.getCachedValue();
+    }
+
+    console.log(`Loading YouTube playlist '${id}' from API`)
+    const response = await youTubeService.playlistItems.list({
+        playlistId: id,
+        part: 'id,snippet',
+        maxResults: 50
+    });
+
+    await asset.save(response, 'json');
+
+    return response;
+};
+
+const getYouTubeVideos = async () => {
+
+    try {
+        const response = await getCachedPlaylist(MESSENGER_MARKETING_PLAYLIST_ID);
+        
         return {
             marketing: response.data
         };
