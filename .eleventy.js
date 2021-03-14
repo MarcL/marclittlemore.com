33c676f5db01e0083aa3c04a98be47e87f3f00c5
Marc Littlemore <marc.littlemore@gmail.com>
2021-03-14 20:02:27 +0000

    Remove layout alias for articles

--- .eleventy.js
+++ .eleventy.js
@@ -27,7 +27,6 @@ module.exports = (eleventyConfig) => {
     // Temporary aliases
     eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
     eleventyConfig.addLayoutAlias('homepage', 'layouts/homepage.html');
-    eleventyConfig.addLayoutAlias('articles', 'layouts/articles.html');
     eleventyConfig.addLayoutAlias('biography', 'layouts/biography.html');
     eleventyConfig.addLayoutAlias('landingpage-default', 'layouts/landingpage-default.html');
     eleventyConfig.addLayoutAlias('landingpage-long', 'layouts/landingpage-long.html');
