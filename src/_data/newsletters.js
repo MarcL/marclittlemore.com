8a7e175860db54087b6644af3734f015618b76cf
Marc Littlemore <marc.littlemore@gmail.com>
2021-05-20 21:59:00 +0100

    Retrieve newsletter metadata

---
+++ src/_data/newsletters.js
@@ -0,0 +1,28 @@
+const Cache = require("@11ty/eleventy-cache-assets");
+const emailOctopusConfig = require('./emailOctopus.json')
+
+// TODO: Add back issue URLs in here eventually
+const backIssues = [];
+
+const MINIMUM_SUBSCRIBERS = 250;
+const EMAIL_OCTOPUS_BASE_URL = 'https://emailoctopus.com/api/1.5';
+
+module.exports = async () => {
+    const {EMAIL_OCTOPUS_API_KEY: apiKey} = process.env;
+    const {listId} = emailOctopusConfig;
+    const url = `${EMAIL_OCTOPUS_BASE_URL}/lists/${listId}?api_key=${apiKey}`;
+
+    const response = await Cache(url, {
+        duration: '1d',
+        type: 'json',
+    });
+
+    const actualSubscribers = response.counts.subscribed;
+    const subscribers = actualSubscribers < MINIMUM_SUBSCRIBERS ?
+        MINIMUM_SUBSCRIBERS : actualSubscribers;
+
+    return {
+        backIssues,
+        subscribers
+    };
+}
--- src/pages/newsletter.md
+++ src/pages/newsletter.md
@@ -11,7 +11,9 @@ image:
     url: https://unsplash.com/@mailchimp
 ---
 
-Testing the text
+Every Sunday, **{{newsletters.subscribers}} technical leaders, developers and creatives** rethink their relationship with money and ambition with my newsletter.
+
+Join our global community now!
 
 {% capture formTitle %}
 Get my **best articles**
@@ -21,9 +23,9 @@ Get my **best articles**
 Sign up for my newsletter to receive great stuff.
 {% endcapture %}
 
-{% include partials/emailOctopus/form.html, formTitle: formTitle, formText: formText %}
+{% include partials/emailOctopus/form.html, formTitle: formTitle, formText: formText, tag: "testing" %}
 
-{% if newsletters.length > 0 %}
+{% if newsletters.backissues.length > 0 %}
 ## Previous newsletters
 
 Want to know what you're going to receive? Check out my previous newsletters.
