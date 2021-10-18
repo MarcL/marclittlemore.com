53b61424456f68e1dec5aedaa7038f7e2bd7343b
Marc Littlemore <MarcL@users.noreply.github.com>
2021-10-18 21:39:30 +0100

    feat: add accessible anchor links for h2 tags (#134)

--- _eleventy/markdown.js
+++ _eleventy/markdown.js
@@ -1,12 +1,59 @@
 const markdownIt = require('markdown-it');
 const markdownItAttributes = require('@gerhobbelt/markdown-it-attrs');
+const string = require('string');
 
 // Create own markdown renderer so we can add class attributes
 // https://www.11ty.dev/docs/languages/markdown/#markdown-options
 const markdownItOptions = {
     html: true,
 };
+
+const slugify = text => string(text).slugify().toString();
+
 const markdownLib = markdownIt(markdownItOptions);
-markdownLib.use(markdownItAttributes);
+
+const linkIcon = '<i class="fas fa-link fa-lg"></i>';
+
+// Render anchor links like GitHub
+const markdownAnchorLinks = (md, options) => {
+  const defaultOptions = {
+    divClass: 'group relative',
+    anchorClass: 'hidden lg:inline opacity-10 group-hover:opacity-100 absolute top-1 -left-10',
+  };
+
+  const anchorOptions = Object.assign({}, defaultOptions, options);
+
+  md.renderer.rules.heading_open = function(tokens, index) {
+    const contentToken = tokens[index + 1];
+    const slug = slugify(contentToken.content);
+
+    if (tokens[index].tag === 'h2') {
+      return `
+      <div class="${anchorOptions.divClass}">
+        <${tokens[index].tag} id="${slug}">`;
+    }
+    return `<${tokens[index].tag}>`;
+  };
+
+  md.renderer.rules.heading_close = function(tokens, index) {
+    const contentToken = tokens[index - 1];
+    const slug = slugify(contentToken.content);
+
+    if (tokens[index].tag === 'h2') {
+      return `
+      </${tokens[index].tag}>
+        <a class="${anchorOptions.anchorClass}" href="#${slug}">
+          <span aria-hidden="true">${linkIcon}</span>
+          <span class="hidden">Permalink to "${contentToken.content}"</span>
+        </a>
+      </div>`;
+    }
+    return `</${tokens[index].tag}>`;
+  };
+};
+
+markdownLib
+    .use(markdownItAttributes)
+    .use(markdownAnchorLinks);
 
 module.exports = markdownLib;
