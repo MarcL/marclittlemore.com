35b9d0e8d5415a1f824683706a4c79f9b0111faa
Marc Littlemore <marc.littlemore@gmail.com>
2021-12-30 16:37:20 +0000

    add gravata shortcode

--- _eleventy/shortcodes.js
+++ _eleventy/shortcodes.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const markdownLib = require('markdown-it')();
 const imageShortcode = require('./image');
 
@@ -31,10 +32,23 @@ const quote = (content, addQuotes = true) => {
     return `<div class="p-4 font-semibold text-xl sm:text-2xl italic border-l-4 border-red-600">${renderedHtml}</div>`;
 };
 
+const gravatarShortcode = (email, size = 80) => {
+
+    // Create an MD5 hash from the email address
+    const emailHash = crypto
+        .createHash('md5')
+        .update(email)
+        .digest('hex');
+
+    // Return a URL image with the hash
+    return `https://www.gravatar.com/avatar/${emailHash}?d=mp&s=${size}`;
+};
+
 const addAll = (eleventyConfig) => {
     eleventyConfig.addPairedShortcode('quote', quote);
     eleventyConfig.addPairedShortcode('callout', callout);
     eleventyConfig.addLiquidShortcode('image', imageShortcode);
+    eleventyConfig.addShortcode('gravatar', gravatarShortcode);
 };
 
 module.exports = addAll;
