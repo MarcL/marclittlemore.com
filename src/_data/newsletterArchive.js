ff2eb9fb9840dfef3af56b72610064a0110eb126
Marc Littlemore <MarcL@users.noreply.github.com>
2022-04-19 12:09:05 +0100

    use Email Octopus API to get newsletters (#181)

--- src/_data/newsletterArchive.js
+++ src/_data/newsletterArchive.js
@@ -1,20 +1,8 @@
 const Cache = require("@11ty/eleventy-cache-assets");
 const emailOctopusConfig = require('./emailOctopus.json')
 
-const archive = [
-    {
-        date: '2022-04-16',
-        title: '#2 - Intentional Technical Management',
-        url: 'https://eocampaign1.com/web-version?p=c0293117-b902-11ec-9258-0241b9615763&pt=campaign&t=1649974143&s=3266ff457b83bcf935a934a8667520562946070817bbfd061dd8a0c0597f09ab'
-    },
-    {
-        date: '2022-04-09',
-        title: '#1 - Intentional Technical Management',
-        url: 'https://eocampaign1.com/web-version?p=94cb337e-b138-11ec-9258-0241b9615763&pt=campaign&t=1649500681&s=3b446ff9d810ff8a09a5c08a6f0a0fb26e37842da31472e01b74b1ab010a2ba9'
-    },
-];
-
 const EMAIL_OCTOPUS_BASE_URL = 'https://emailoctopus.com/api/1.6';
+const EMAIL_OCTOPUS_WEB_VIEW_URL = 'https://eocampaign1.com/web-version';
 
 module.exports = async () => {
     const {EMAIL_OCTOPUS_API_KEY: apiKey} = process.env;
@@ -33,7 +21,15 @@ module.exports = async () => {
 
     // Retrieve newsletters that match the list ID for this site
     // in case I start another!
-    const newsletters = response.data.filter(newsletter => newsletter.to.includes(listId));
+    const newsletters = response.data.filter(newsletter => newsletter.to.includes(listId))
+        .map(newsletter => {
+            const {id, name, content, sent_at: sent} = newsletter;
+            return {
+                date: sent,
+                title: name,
+                url: `${EMAIL_OCTOPUS_WEB_VIEW_URL}?p=${id}&pt=campaign`
+            };
+        });
 
-    return archive;
+    return newsletters;
 };
