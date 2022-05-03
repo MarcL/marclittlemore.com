045d532c508be3c1d3aa344f86af29f777ff3c47
Marc Littlemore <marc.littlemore@gmail.com>
2022-05-03 21:53:23 +0100

    add newsletter pages

--- src/_data/newsletterArchive.js
+++ src/_data/newsletterArchive.js
@@ -4,6 +4,16 @@ const emailOctopusConfig = require('./emailOctopus.json')
 const EMAIL_OCTOPUS_BASE_URL = 'https://emailoctopus.com/api/1.6';
 const EMAIL_OCTOPUS_WEB_VIEW_URL = 'https://eocampaign1.com/web-version';
 
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    
+    const locale = 'en-US';
+    const day = date.toLocaleDateString(locale, { day: '2-digit' });
+    const month = date.toLocaleDateString(locale, { month: '2-digit' });
+    const year = date.toLocaleDateString(locale, { year: 'numeric' });
+    return `${year}-${month}-${day}`;
+};
+
 module.exports = async () => {
     const {EMAIL_OCTOPUS_API_KEY: apiKey} = process.env;
 
@@ -23,13 +33,15 @@ module.exports = async () => {
     // in case I start another!
     const newsletters = response.data.filter(newsletter => newsletter.to.includes(listId))
         .map(newsletter => {
-            const {id, name, status, created_at: created, sent_at: sent} = newsletter;
+            const {id, name, status, created_at: created, sent_at: sent, content} = newsletter;
             return {
                 created,
                 sent,
+                sentSlug: formatDate(sent),
                 title: name,
                 url: `${EMAIL_OCTOPUS_WEB_VIEW_URL}?p=${id}&pt=campaign`,
                 status,
+                content
             };
         })
         .filter(newsletter => newsletter.status.toLowerCase() === 'sent');
