c82f56d740d2972ea6bdddda5385468a83fb148e
Marc Littlemore <marc.littlemore@gmail.com>
2022-05-17 10:08:34 +0100

    tidy up site metadata

--- src/_data/site.js
+++ src/_data/site.js
@@ -12,18 +12,14 @@ const url = isDevelopmentBuild ? 'http://localhost:8080' : netlifyBuildUrl;
 module.exports = {
   title: 'Marc Littlemore',
   headerImage: '/images/banners/home-bg.jpg',
-  name: 'Marc Littlemore',
-  email: 'marc@marclittlemore.com',
   description: 'Interesting articles from Marc Littlemore, an engineering manager, technical leader, full-stack developer, teacher, and life-long learner.',
   canonicalUrl: CANONICAL_URL,
   url,
-  domain: 'www.marclittlemore.com',
   googleAnalyticsId: 'UA-810717-3',
   twitterUsername: 'marclittlemore',
-  author: 'https://www.facebook.com/marcdavidlittlemore',
   facebookAppId: '534953096696612',
   author: {
     name: 'Marc Littlemore',
-    email: 'marc@marclittlemore.com',
+    email: 'marc@marclittlemore.com'
   }
 };
