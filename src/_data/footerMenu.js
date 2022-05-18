14665c3bdc7f2c6a02a90738fe764469f98a6e9e
Marc Littlemore <MarcL@users.noreply.github.com>
2022-05-18 10:58:43 +0100

    refactor: footer (#193)

---
+++ src/_data/footerMenu.js
@@ -0,0 +1,53 @@
+const popularPages = require('./popularPages');
+
+module.exports = [
+    {
+        name: 'Hot links',
+        links: [
+            {
+                name: 'Newsletter',
+                url: '/newsletter/',
+                title: 'Sign up for my intentional technical leadership newsletter'
+            },
+            {
+                name: 'JavaScript Testing',
+                url: '/javascript-testing/',
+                title: 'Learn more about testing your JavaScript code'
+            },
+            {
+                name: 'Chatbots',
+                url: '/bots/',
+                title: 'Learn how to create your own chatbot'
+            },
+            {
+                name: 'Now',
+                url: '/now/',
+                title: 'What I\'m doing right now'
+            },
+            {
+                name: 'Uses',
+                url: '/uses/',
+                title: 'Applications and hardware I use everyday'
+            }
+        ]    
+    },
+    {
+        name: 'Popular pages',
+        links: popularPages 
+    },
+    {
+        name: 'More',
+        links: [
+            {
+                name: 'Contact me',
+                url: '/contact/',
+                title: 'Send me a message and say hello'
+            },
+            {
+                name: 'About me',
+                url: '/about/',
+                title: 'Who is Marc Littlemore?'
+            },
+        ] 
+    }
+];
---
+++ src/_data/popularPages.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        name: 'Obsidian beginner\'s guide',
+        url: '/beginners-guide-note-taking-obsidian/',
+        title: 'A beginner\'s guide to note-taking using Obsidian'
+    },
+    {
+        name: 'How I almost died',
+        url: '/how-i-almost-died/',
+        title: 'Read about how I almost died due to sepsis'
+    },
+    {
+        name: 'Unit testing Express routes',
+        url: '/how-to-unit-test-express-routes/',
+        title: 'How to unit test Express routes in JavaScript'
+    },
+    {
+        name: 'Build a serverless Telegram bot',
+        url: '/serverless-telegram-chatbot-vercel/',
+        title: 'Build a serverless Telegram bot and host it on Vercel'
+    },
+    {
+        name: 'High quality unit tests',
+        url: '/how-to-write-high-quality-unit-tests/',
+        title: 'Learn how to write high quality unit tests'
+    },
+];
