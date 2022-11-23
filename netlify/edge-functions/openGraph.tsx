46747da13c1b28ea90cc45e7de5a318dcc714e9f
Marc Littlemore <marc.littlemore@gmail.com>
2022-11-23 09:01:40 +0000

    feat: add open graph edge generation as an edge function

---
+++ netlify/edge-functions/openGraph.tsx
@@ -0,0 +1,173 @@
+import React from 'https://esm.sh/react@18.2.0?deno-std=0.140.0'
+import { ImageResponse } from 'https://deno.land/x/og_edge@0.0.2/mod.ts'
+
+const BACKGROUND_IMAGE_STD =
+  'https://obuldanrptloktxcffvn.supabase.co/storage/v1/object/public/images/lw6/lw6_ticket_regular.jpg'
+const BACKGROUND_IMAGE_GOLDEN =
+  'https://obuldanrptloktxcffvn.supabase.co/storage/v1/object/public/images/lw6/lw6_ticket_gold.jpg'
+const SUPA_CHECKMARK =
+  'https://obuldanrptloktxcffvn.supabase.co/storage/v1/object/public/images/lw6/supaverified.png'
+
+const corsHeaders = {
+'Access-Control-Allow-Origin': '*',
+'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey',
+};
+
+export default async function handler(req: Request) {
+  const url = new URL(req.url)
+  const ticketNumber = url.searchParams.get('ticketNumber')
+  const username = url.searchParams.get('username') ?? url.searchParams.get('amp;username')
+  const name = url.searchParams.get('name') ?? url.searchParams.get('amp;name')
+  const golden = url.searchParams.get('golden') ?? url.searchParams.get('amp;golden')
+
+  if (!username || !ticketNumber || !name)
+    return new Response(JSON.stringify({ error: 'missing params' }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+
+  const numDigits = `${Number(ticketNumber)}`.length
+  const prefix = `00000000`.slice(numDigits)
+
+  try {
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            width: '2000px',
+            height: '1000px',
+            backgroundColor: '#333',
+            color: '#fff',
+            overflow: 'hidden',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {/* Background image  */}
+          {/* <img
+            width="2000"
+            height="1000"
+            style={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              zIndex: '-9000',
+            }}
+            src={golden ? BACKGROUND_IMAGE_GOLDEN : BACKGROUND_IMAGE_STD}
+          /> */}
+          {/* GitHub Avatar image */}
+          <img
+            width="270"
+            height="270"
+            style={{
+              position: 'absolute',
+              top: '360',
+              left: '460',
+              borderRadius: 135,
+            }}
+            src={`https://github.com/${username}.png`}
+          />
+          {/* Name & username */}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              position: 'absolute',
+              top: '360',
+              left: '815',
+              width: '600',
+              height: '270',
+              overflow: 'hidden',
+              textOverflow: 'clip',
+            }}
+          >
+            <p
+              style={{
+                fontSize: '70',
+                lineHeight: 0.9,
+              }}
+            >
+              {name}
+            </p>
+            {/* Username and supaverified checkmark */}
+            <div
+              style={{
+                display: 'flex',
+                fontSize: '35',
+              }}
+            >
+              <span>{`@${username}`}</span>
+              <span
+                style={{
+                  marginTop: '5',
+                  marginLeft: '10',
+                }}
+              >
+                <img width="44" height="44" src={SUPA_CHECKMARK} />
+              </span>
+            </div>
+          </div>
+          {/* Date  */}
+          <p
+            style={{
+              position: 'absolute',
+              top: '750',
+              left: '815',
+              fontSize: '25',
+            }}
+          > 
+            {new Date().toLocaleDateString('en-US', { dateStyle: 'medium'})}
+          </p>
+          {/* URL  */}
+          <p
+            style={{
+              position: 'absolute',
+              top: '750',
+              left: '1120',
+              fontSize: '25',
+            }}
+          >
+            marclittlemore.com
+          </p>
+          {/* Ticket No  */}
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              position: 'absolute',
+              bottom: '385',
+              right: '60',
+              width: '705',
+              height: '215',
+              transform: 'rotate(90deg)',
+            }}
+          >
+            <p
+              style={{
+                fontSize: '80',
+              }}
+            >
+              {`№ ${prefix}${ticketNumber}`}
+            </p>
+          </div>
+        </div>
+      ),
+      {
+        width: 2000,
+        height: 1000,
+        headers: {
+          'content-type': 'image/png',
+          'cache-control': 'public, max-age=31536000, no-transform, immutable',
+        },
+      }
+    )
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+}
