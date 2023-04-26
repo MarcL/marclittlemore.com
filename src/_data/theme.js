e30b5b45988b8976441facdd130c227fcd203560
Marc Littlemore <marc.littlemore@gmail.com>
2023-04-26 21:40:57 +0100

    initial theme

---
+++ src/_data/theme.js
@@ -0,0 +1,23 @@
+module.exports = {
+    page: {
+        background: 'bg-white',
+        text: {
+            title: 'text-gray-900',
+            subtitle: 'text-gray-900',
+            content: 'text-gray-800',
+        },
+        sectionDark: {
+            background: 'bg-gray-700 text-gray-800',
+            text: {
+                title: 'text-white',
+                link: 'text-white',
+            }
+        },
+        sectionLight: {
+            background: 'bg-gray-100 text-gray-800',
+            text: {
+                title: 'text-gray-800',
+            }
+        }
+    }
+};
