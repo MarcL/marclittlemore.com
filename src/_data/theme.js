abf98e315bfc63b6a8e7bed12131b8271ddb9521
Marc Littlemore <marc.littlemore@gmail.com>
2023-04-26 23:09:31 +0100

    more tweaks

--- src/_data/theme.js
+++ src/_data/theme.js
@@ -3,21 +3,21 @@ module.exports = {
         font: 'font-sans antialiased',
         background: 'bg-white',
         text: {
-            title: 'text-gray-900',
-            subtitle: 'text-gray-600',
-            content: 'text-gray-800',
+            title: 'text-slate-900',
+            subtitle: 'text-slate-600',
+            content: 'text-slate-800',
         },
         sectionDark: {
-            background: 'bg-gray-700 text-gray-800',
+            background: 'bg-slate-700 text-slate-800',
             text: {
                 title: 'text-white',
-                link: 'text-white hover:text-gray-200 hover:underline',
+                link: 'text-white hover:text-slate-200 hover:underline',
             }
         },
         sectionLight: {
-            background: 'bg-gray-100 text-gray-800',
+            background: 'bg-slate-100 text-slate-800',
             text: {
-                title: 'text-gray-800',
+                title: 'text-slate-800',
             }
         },
         metadata: {
@@ -28,13 +28,13 @@ module.exports = {
         },
         navigation: {
             text: {
-                main: 'text-gray-900',
+                main: 'text-slate-900',
                 selected: 'border-b-4 border-red-500',
-                hover: 'border-white hover:border-red-500 hover:text-gray-700'
+                hover: 'border-white hover:border-red-500 hover:text-slate-700'
             }
         },
         footer: {
-            background: 'bg-gradient-to-b from-gray-700 to-gray-900',
+            background: 'bg-gradient-to-b from-slate-700 to-slate-900',
             column: {
                 title: 'text-white',
                 content: 'text-slate-100 hover:underline hover:text-slate-200',
@@ -50,40 +50,46 @@ module.exports = {
     },
     article: {
         text: {
-            title: 'text-gray-900 font-semibold',
+            title: 'text-slate-900 font-semibold',
             content: 'text-slate-600',
             hover: 'hover:decoration-red-500 hover:underline hover:decoration-red-500 hover:underline-offset-4'
         },
         icons: {
             main: 'text-red-700'
+        },
+        image: {
+            hover: 'hover:contrast-125'
         }
     },
     bookshelf: {
         text: {
-            title: 'text-gray-900',
-            author: 'text-gray-600',
-            content: 'text-gray-600',
+            title: 'text-slate-900',
+            author: 'text-slate-600',
+            content: 'text-slate-600',
             star: 'text-amber-400'
         }
     },
     card: {
         background: 'bg-white',
         text: {
-            content: 'text-gray-500',
+            content: 'text-slate-600',
             link: 'underline hover:text-red-700'
+        },
+        hover: {
+            image: 'hover:brightness-125'
         }
     },
     contact: {
         background: 'bg-white',
         text: {
-            label: 'text-gray-700'
+            label: 'text-slate-700'
         },
         button: {
             background: 'bg-red-600',
             text: 'hover:bg-red-700 text-white font-bold'
         },
         input: {
-            background: 'border-gray-300 focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50'
+            background: 'border-slate-300 focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50'
         }
     }
 };
