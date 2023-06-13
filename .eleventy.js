e7d13e352f0086f5a14d6fc9eafea35800f7589e
Marc Littlemore <marc.littlemore@gmail.com>
2023-06-13 21:07:49 +0100

    feat: refactor all the things

--- .eleventy.js
+++ .eleventy.js
@@ -21,7 +21,7 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addPlugin(embedYouTube);
     eleventyConfig.addPlugin(embedTwitter);
     eleventyConfig.addPlugin(timeToRead, {style: 'short'});
-    eleventyConfig.addPlugin(tableOfContents);
+    eleventyConfig.addPlugin(tableOfContents, {tags: ['h2']});
     eleventyConfig.addPlugin(externalLinks, {
         url: site.url,
         overwrite: false,
