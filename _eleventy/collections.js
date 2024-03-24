d2092dd1c496be4984237159b00526fb33dac5ea
Marc Littlemore <MarcL@users.noreply.github.com>
2024-03-24 17:47:12 +0000

    refactor: collections (#402)
    * move collections
    
    * refactor config

---
+++ _eleventy/collections.js
@@ -0,0 +1,28 @@
+const addCollections = (eleventyConfig) => {
+    // Tag list for specific tag pages
+    eleventyConfig.addCollection('tagList', (collection) => {
+        let tagSet = new Set();
+    
+        // Ignore these tags
+        const filteredTags = ['post', 'talk', 'note'];
+        collection.getAll().forEach((item) => {
+          if (!item.data.tags) {
+            return;
+          }
+    
+          item.data.tags
+            .filter((tag) => !filteredTags.includes(tag))
+            .forEach((tag) => tagSet.add(tag));
+        });
+    
+        return [...tagSet].sort((first, second) => first.localeCompare(second));
+    });  
+
+    // RSS feed
+    // - Only add posts for now
+    eleventyConfig.addCollection('feed', (collection) => {
+      return collection.getFilteredByGlob(['src/posts/**/*.md']);
+    });
+  };
+  
+  module.exports = addCollections;
