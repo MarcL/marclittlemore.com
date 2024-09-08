10e9a01d90049d2931d2dbe08314d9ac62ef97af
Marc Littlemore <marc.littlemore@gmail.com>
2024-09-08 16:14:23 +0100

    feat: make notes a bit prettier

--- src/_data/goodreadsBooks.js
+++ src/_data/goodreadsBooks.js
@@ -1,10 +1,10 @@
-const Cache = require("@11ty/eleventy-cache-assets");
+const Cache = require('@11ty/eleventy-cache-assets');
 const xml2js = require('xml2js');
 const books = require('./books');
 
 const extractGoodreadsBookMetadata = (goodReadsBook) => {
-    const {rating, read_at} = goodReadsBook
-    const {link, uri, title, image_url, authors} = goodReadsBook.book;
+    const { rating, read_at } = goodReadsBook;
+    const { link, uri, title, image_url, authors } = goodReadsBook.book;
     const finishedOn = read_at ? new Date(read_at) : null;
     return {
         uri,
@@ -14,13 +14,13 @@ const extractGoodreadsBookMetadata = (goodReadsBook) => {
         rating,
         authorName: authors.author.name,
         authorLink: authors.author.link,
-        finishedOn
-    }
+        finishedOn,
+    };
 };
 
 const extractReadingListMetadata = (list) => {
     const extractList = Array.isArray(list) ? list : [list];
-    
+
     return extractList.map(extractGoodreadsBookMetadata);
 };
 
@@ -33,14 +33,15 @@ const createGoodReadsUrl = (options) => {
 
 const getGoodreadsShelf = async (shelfName) => {
     try {
-        const {GOODREADS_USER_ID: id, GOODREADS_API_KEY: apiKey} = process.env;
-    
+        const { GOODREADS_USER_ID: id, GOODREADS_API_KEY: apiKey } =
+            process.env;
+
         const searchParams = {
             v: 2,
             id,
             key: apiKey,
             shelf: shelfName,
-            per_page: 200
+            per_page: 200,
         };
 
         const url = createGoodReadsUrl(searchParams);
@@ -50,44 +51,47 @@ const getGoodreadsShelf = async (shelfName) => {
             type: 'text',
             fetchOptions: {
                 headers: {
-                    Accept: 'application/xhtml+xml'
-                }
-            }
+                    Accept: 'application/xhtml+xml',
+                },
+            },
         });
 
-        const parser = xml2js.Parser({explicitArray: false});
+        const parser = xml2js.Parser({ explicitArray: false });
         const data = await parser.parseStringPromise(response);
 
-        return extractReadingListMetadata(data.GoodreadsResponse.reviews.review);
-    }
-    catch(error) {
+        return extractReadingListMetadata(
+            data.GoodreadsResponse.reviews.review
+        );
+    } catch (error) {
         console.error(error.toString());
         return [];
     }
 };
 
 const updateBooks = (bookList) => {
-    return bookList.map(book => {
-        const {uri} = book;
-        const updatedBook = books.find(updated => updated.uri === uri);
+    return bookList.map((book) => {
+        const { uri } = book;
+        const updatedBook = books.find((updated) => updated.uri === uri);
         const newBook = {
             ...book,
-            ...updatedBook
+            ...updatedBook,
         };
 
         return newBook;
     });
 };
 
-
 const getGoodreadsBooks = async () => {
     const currentlyReading = await getGoodreadsShelf('currently-reading');
     const read = await getGoodreadsShelf('read');
-    read.sort((first, second) => new Date(second.finishedOn) - new Date(first.finishedOn));
+    read.sort(
+        (first, second) =>
+            new Date(second.finishedOn) - new Date(first.finishedOn)
+    );
 
     const books = {
         currentlyReading: updateBooks(currentlyReading),
-        read: updateBooks(read)
+        read: updateBooks(read),
     };
 
     return books;
