c8c55586cd075cba4b68cf3fcaeb6d74e11eb5a5
Marc Littlemore <MarcL@users.noreply.github.com>
2024-10-12 20:44:14 +0100

    chore: update to 11ty v3 (#514)
    * chore: update to 11ty v3

--- .eleventy.js
+++ .eleventy.js
@@ -6,9 +6,9 @@ const embedYouTube = require('eleventy-plugin-youtube-embed');
 const embedTwitter = require('eleventy-plugin-embed-twitter');
 const timeToRead = require('eleventy-plugin-time-to-read');
 const tableOfContents = require('eleventy-plugin-nesting-toc');
-const externalLinks = require('@aloskutov/eleventy-plugin-external-links')
+const externalLinks = require('@aloskutov/eleventy-plugin-external-links');
 
-const util = require('util');
+const util = require('node:util');
 
 const addAllShortcodes = require('./_eleventy/shortcodes');
 const addAllFilters = require('./_eleventy/filters');
@@ -17,71 +17,73 @@ const site = require('./src/_data/site');
 const addCollections = require('./_eleventy/collections');
 
 module.exports = (eleventyConfig) => {
-  // Plugins
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(embedYouTube);
-  eleventyConfig.addPlugin(embedTwitter);
-  eleventyConfig.addPlugin(timeToRead, { style: 'short' });
-  eleventyConfig.addPlugin(tableOfContents, { tags: ['h2'] });
-  eleventyConfig.addPlugin(externalLinks, {
-    url: site.url,
-    overwrite: false,
-    rel: ['noreferrer', 'noopener', 'external'],
-  });
+    // Plugins
+    eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(embedYouTube);
+    eleventyConfig.addPlugin(embedTwitter);
+    eleventyConfig.addPlugin(timeToRead, { style: 'short' });
+    eleventyConfig.addPlugin(tableOfContents, { tags: ['h2'] });
+    eleventyConfig.addPlugin(externalLinks, {
+        url: site.url,
+        overwrite: false,
+        rel: ['noreferrer', 'noopener', 'external'],
+    });
 
-  // Collections
-  addCollections(eleventyConfig);
+    // Collections
+    addCollections(eleventyConfig);
 
-  // RSS
-  eleventyConfig.addPlugin(rss);
-  eleventyConfig.addLiquidFilter('dateToRfc3339', rss.dateToRfc3339);
-  eleventyConfig.addLiquidFilter(
-    'getNewestCollectionItemDate',
-    rss.getNewestCollectionItemDate
-  );
-  eleventyConfig.addLiquidFilter('absoluteUrl', rss.absoluteUrl);
-  eleventyConfig.addLiquidFilter(
-    'convertHtmlToAbsoluteUrls',
-    rss.convertHtmlToAbsoluteUrls
-  );
+    // RSS
+    eleventyConfig.addPlugin(rss);
+    eleventyConfig.addLiquidFilter('dateToRfc3339', rss.dateToRfc3339);
+    eleventyConfig.addLiquidFilter(
+        'getNewestCollectionItemDate',
+        rss.getNewestCollectionItemDate
+    );
+    eleventyConfig.addLiquidFilter('absoluteUrl', rss.absoluteUrl);
+    eleventyConfig.addLiquidFilter(
+        'convertHtmlToAbsoluteUrls',
+        rss.convertHtmlToAbsoluteUrls
+    );
 
-  eleventyConfig.setLibrary('md', markdownLib);
+    eleventyConfig.setLibrary('md', markdownLib);
 
-  // Copy
-  eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
-  eleventyConfig.addPassthroughCopy({ 'src/favicons': 'favicons' });
-  eleventyConfig.addPassthroughCopy({ 'src/robots.txt': 'robots.txt' });
-  eleventyConfig.addPassthroughCopy({ 'src/ads.txt': 'ads.txt' });
-  eleventyConfig.addPassthroughCopy({
-    'src/browserconfig.xml': 'browserconfig.xml',
-  });
-  eleventyConfig.addPassthroughCopy({ 'src/distjs/': 'js/' });
-  eleventyConfig.addPassthroughCopy({ './_tmp/css/main.css': 'css/main.css' });
-  eleventyConfig.addPassthroughCopy({ 'src/_redirects': '_redirects' });
+    // Copy
+    eleventyConfig.addPassthroughCopy({ 'src/images': 'images' });
+    eleventyConfig.addPassthroughCopy({ 'src/favicons': 'favicons' });
+    eleventyConfig.addPassthroughCopy({ 'src/robots.txt': 'robots.txt' });
+    eleventyConfig.addPassthroughCopy({ 'src/ads.txt': 'ads.txt' });
+    eleventyConfig.addPassthroughCopy({
+        'src/browserconfig.xml': 'browserconfig.xml',
+    });
+    eleventyConfig.addPassthroughCopy({ 'src/distjs/': 'js/' });
+    eleventyConfig.addPassthroughCopy({
+        './_tmp/css/main.css': 'css/main.css',
+    });
+    eleventyConfig.addPassthroughCopy({ 'src/_redirects': '_redirects' });
 
-  // Watch - including files in .gitignore
-  eleventyConfig.setUseGitIgnore(false);
-  eleventyConfig.addWatchTarget('./src/distjs/');
-  eleventyConfig.addWatchTarget('./_tmp/css/main.css');
+    // Watch - including files in .gitignore
+    eleventyConfig.setUseGitIgnore(false);
+    eleventyConfig.addWatchTarget('./src/distjs/');
+    eleventyConfig.addWatchTarget('./_tmp/css/main.css');
 
-  addAllFilters(eleventyConfig);
-  addAllShortcodes(eleventyConfig);
+    addAllFilters(eleventyConfig);
+    addAllShortcodes(eleventyConfig);
 
-  // Debug filter
-  eleventyConfig.addFilter('console', (value) => {
-    return util.inspect(value);
-  });
+    // Debug filter
+    eleventyConfig.addFilter('console', (value) => {
+        return util.inspect(value);
+    });
 
-  eleventyConfig.setDataDeepMerge(true);
+    eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.setLiquidOptions({
-    dynamicPartials: false,
-  });
+    eleventyConfig.setLiquidOptions({
+        dynamicPartials: false,
+    });
 
-  return {
-    dir: {
-      input: './src',
-      output: './_site',
-    },
-  };
+    return {
+        dir: {
+            input: './src',
+            output: './_site',
+        },
+    };
 };
